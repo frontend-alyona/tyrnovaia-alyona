9271bb2573b0056b1ecf73429f93622dc13fb543
frontend-alyona <gluzhetskaya@gmail.com>
2021-06-24 20:26:39 +0300

    change user age

---
+++ script/script.js
@@ -0,0 +1,19 @@
+let userAge = 10;
+
+
+let userName ="Alena";
+
+
+let userSurname = "Tyrnovaia";
+
+let userNew = "new";
+
+let listProducts = "products";
+let userOnline = "online";
+
+let salaryWorker = "salary";
+
+let cursorCoordinates = "cursor";
+
+const greeting = 'Hello world';
+console.log('greeting', greeting);
