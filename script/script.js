6b23dd58d48681d1b6a8c67925e05092c6405e25
frontend-alyona <gluzhetskaya@gmail.com>
2021-06-30 12:07:25 +0300

    add new variables in task 1

--- script/script.js
+++ script/script.js
@@ -1,21 +1,89 @@
-let userAge = 10;
+// homework 2
+// let userAge = 10;
 
-let userage = 25;
-
-
-let userName ="Alena";
+// let userage = 25;
 
 
-let userSurname = "Tyrnovaia";
+// let userName ="Alena";
 
-let userNew = "new";
 
-let listProducts = "products";
-let userOnline = "online";
+// let userSurname = "Tyrnovaia";
 
-let salaryWorker = "salary";
+// let userNew = "new";
 
-let cursorCoordinates = "cursor";
+// let listProducts = "products";
+// let userOnline = "online";
 
-const greeting = 'Hello world';
-console.log('greeting', greeting);
+// let salaryWorker = "salary";
+
+// let cursorCoordinates = "cursor";
+
+// const greeting = 'Hello world';
+// console.log('greeting', greeting);
+
+
+
+// homework 3
+// task 1
+let userFruit = 10;
+let userVegetables = 20;
+
+const fruitAmount = Number(prompt('How much fruits?'));
+
+const vegetablesAmount = Number(prompt('How much vegetables?'));
+console.log('10<20', 10<20);
+if(userAnswer = 10<20){
+    alert ('10 меньше чем 20');
+}   
+
+
+// task2
+let weeks = 7;
+let months = 12;
+
+const daysInAWeek = Number(prompt('How many days in a week?'));
+switch (daysInAWeek){
+    case 7:
+    alert ('Right!');
+    break;
+    default:
+    alert('Think a little bit more');  
+
+
+}
+const monthsInAYear = Number(prompt('How many months in a year?'));
+switch (monthsInAYear){
+    case 12:
+    alert ('You are right!');
+    break;
+    default:
+    alert('NOOOO!');
+}
+ const num1 = 7;
+ const num2 = 12;
+ let result;
+ result = num1 + num2;
+ console.log ('7+12', result);
+ alert('19');
+
+// task3
+const minAge = 12-18;
+const maxAge = 60-80;
+
+const visitorAge = Number(prompt('How old are you?'));
+const areYouWithParents = confirm('Are you with parents?');
+
+if (visitorAge >= minAge && maxAge || areYouWithParents ) {
+    console.log ('Access is allowed');
+} else {
+    console.log ('Access is denied');
+}
+
+
+
+
+
+
+
+
+
