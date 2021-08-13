43e2c1c728cf18ef3b07233d2b4e4b995cc4f8c8
frontend-alyona <gluzhetskaya@gmail.com>
2021-08-13 16:50:51 +0300

    fixed task 3

--- homework8/script.js
+++ homework8/script.js
@@ -2,20 +2,20 @@
 
 
 
-const findTrue  = (ar) => {
-  let quantity = 0;
-  ar.filter((i) => {
-    if (i === true) { quantity++ }
-  })
-  console.log(quantity)
-  }
+// const findTrue  = (ar) => {
+//   let quantity = 0;
+//   ar.filter((i) => {
+//     if (i === true) { quantity++ }
+//   })
+//   console.log(quantity)
+//   }
    
   
-  findTrue([true, false, false, true, false]); 
+//   findTrue([true, false, false, true, false]); 
   
-  findTrue([false, false, false, false]); 
+//   findTrue([false, false, false, false]); 
   
-  findTrue([]); 
+//   findTrue([]); 
 
 
  
@@ -24,24 +24,23 @@ const findTrue  = (ar) => {
 
 // task 2 +
 
-const arr = ['ab','pq','mn','ab','mn','ab', 'ab', 'mn']
+// const arr = ['ab','pq','mn','ab','mn','ab', 'ab', 'mn']
 
-const elemQuantity = (arr) => {
-  const occurrences = { };
-for (let i = 0, j = arr.length; i < j; i++) {
-   occurrences[arr[i]] = (occurrences[arr[i]] || 0) + 1;
-}
-console.log(occurrences);        
-console.log(occurrences['ab']); 
-}
+// const elemQuantity = (arr) => {
+//   const occurrences = { };
+// for (let i = 0, j = arr.length; i < j; i++) {
+//    occurrences[arr[i]] = (occurrences[arr[i]] || 0) + 1;
+// }
+// console.log(occurrences);        
+// console.log(occurrences['ab']); 
+// }
 
-elemQuantity(arr);
+// elemQuantity(arr);
 
 // task 3 +
 
-function getOddNum (array) {
-  let findOddNum;
-  let odd = [];
+function getExtraN (array) {
+ let odd = [];
   let even = [];
 
   array.forEach(function(i) {
@@ -52,15 +51,18 @@ function getOddNum (array) {
   if (odd.length !== 1 && even.length !== 1) {
   } else {
     findOddNum = (odd.length === 1 ? odd[0] : even[0]);
+
+    
   }
-  console.log(findOddNum);
+  // console.log(findOddNum);
+  return findOddNum;
 }
 
-getOddNum([0, 1, 2]); 
-getOddNum([1, 2, 3]); 
-getOddNum([2, 6, 8, 10, 3]); 
-getOddNum([0, 0, 3, 0, 0]); 
-getOddNum([1, 1, 0, 1, 1]); 
+getExtraN([0, 1, 2]); 
+getExtraN([1, 2, 3]); 
+getExtraN([2, 6, 8, 10, 3]); 
+getExtraN([0, 0, 3, 0, 0]); 
+getExtraN([1, 1, 0, 1, 1]); 
 
 
 
