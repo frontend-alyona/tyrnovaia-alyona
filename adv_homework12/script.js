b085a18862c2953f7e86ffc02e56b1a91231bb10
frontend-alyona <gluzhetskaya@gmail.com>
2021-08-20 23:00:49 +0300

    add changes to hw

--- adv_homework12/script.js
+++ adv_homework12/script.js
@@ -1,22 +1,34 @@
 
 
 const createTooltip =() => {
-    tooltipElem = document.createElement('div');
-    tooltipElem.className = 'tooltip';
-    
-    document.body.append(tooltipElem);
+    tooltipContainer = document.createElement('div');
+     tooltipButton = document.createElement('button');
+     tooltipButton.style = "position:absolute; background: red; padding: 20px 40px";
+     tooltipButton.className = "tooltip";
 
+    tooltipContainer.append(tooltipButton);
     
-    const tooltip = document.querySelector('.tooltip')
-    const buttonCoords = tooltip.getBoundingClientRect();
+    document.body.append(tooltipContainer);
+
+
+    const buttonCoords = button.getBoundingClientRect();
     console.log(buttonCoords);
 
 
-       
+    tooltipButton.style.left = '215px';
+    tooltipButton.style.top = '150px';
+    
+
+    const tooltipCoords = tooltipButton.getBoundingClientRect();
+    console.log(tooltipCoords);
+      
 }
 
 
 createTooltip( );
+
+
+
 
 
 
