cd1742866f1cfb4220780007fe72adc392f6358b
frontend-alyona <gluzhetskaya@gmail.com>
2021-08-24 16:17:59 +0300

    add homework

--- homework9_arr_methods/script.js
+++ homework9_arr_methods/script.js
@@ -1,110 +1,202 @@
 // task 1 
 
-// const cars = [
-//         { mark: 'Ford',
-//             model: 'Focus',
-//             year: 2009 
-//         },
+const cars = [
+  { 
+    mark: 'Ford',
+    model: 'Focus',
+    year: 2009 
+  },
 
-//         { mark: 'Chevrole',
-//             model: 'Epica',
-//             year: 2015
-//          },
+  { 
+    mark: 'Chevrole',
+    model: 'Epica',
+    year: 2015
+  },
 
-//          {
-//             mark: 'Honda',
-//             model: 'Civik',
-//             year: 2015
-//         },
+ {
+    mark: 'Honda',
+    model: 'Civik',
+    year: 2015
+  },
     
-//         {
-//             mark: 'Mitsubishi',
-//             model: 'Lancer',
-//             year: 2009
+  {
+    mark: 'Mitsubishi',
+    model: 'Lancer',
+    year: 2009
     
-//         },
+  },
     
-//         {
-//             mark: 'Subaru',
-//             model: 'Impreza',
-//             year: 2020
-//         },
+  {
+    mark: 'Subaru',
+    model: 'Impreza',
+    year: 2020
+  },
     
-//         {
-//             mark: 'Ford',
-//             model: 'Mustang',
-//             year: 2015
-//         }
+  {
+    mark: 'Ford',
+    model: 'Mustang',
+    year: 2015
+ }
         
-//        ],
+]
+const filterData = (arr, filter) => {
+  return arr.filter(item =>
+      Object.keys(filter).every(key =>
+          item[key] === filter[key]
+      )
+  );
+};
 
-//      car = cars.filter(car => car.year === 2015)  
-//     // car = cars.filter(car => car.mark === 'Ford' )
-//     console.log(car) 
+filterData(cars,{year: 2015});
+console.log(filterData(cars,{year: 2015}));
 
-    
-    
+filterData(cars,{mark: 'Subaru',year: 2020});
+console.log(filterData(cars,{mark: 'Subaru',year: 2020}));
+
 
 // task 2
+const isNegative = (number) => number < 0;
+const increment = (number) => number + 1;
+const logger = (element, index, array) => {
+    console.log(`In array [${array}] on position ${index}: ${element}`);
+};
 
-// const friends = [
-//     {  name: 'Joi Tribbiani',
-//        age: 32,
-//        sex: 'm' 
-//     },
+// filter
+const ownFilter = (arr, isNegative) => {
+  let result = [];
+  for (let key in arr) {
+      let number = arr[key];
+      if (isNegative(number)) {
+          result.push(number)
+      }
+  }
+  return result;
+};
 
-//     {  name: 'Chandler Bing',
-//        age: 33,
-//        sex: 'm'
-//      },
+ownFilter([-2, 4, -1, 6, -5], isNegative);
+console.log(ownFilter([-2, 4, -1, 6, -5], isNegative));
 
-//     {  name: 'Ross Geller',
-//        age: 33,
-//        sex: 'm'
-//      },
+// map
+const ownMap = (arr, increment) => {
+  let result = [];
+  for (let key in arr) {
+      let number = arr[key];
+      number = increment(number);
+      result.push(number);
+  }
+  return result;
+};
 
-//     {  name: 'Rachel Green',
-//        age: 31,
-//        sex: 'f'
-//      },
+ownMap([1, 2, 3, 4, 5], increment);
+console.log(ownMap([1, 2, 3, 4, 5], increment));
 
-//     {  name: 'Monica Geller',
-//        age: 31,
-//        sex: 'f'
-//      },
+// forEach
+const ownForEach = (array, logger) => {
+  for (let index in array) {
+      let element = array[index];
+      logger(element, index, array);
+  }
+};
 
-//     {  name: 'Phoebe Buffay',
-//        age: 34,
-//        sex: 'f'
-//      },
-//    ]
-
-//    const age = [];
-// friends.forEach(friend => age.push(friend.age));
-// console.log(age);
-
-// const sex = friends.map(friend => friend.sex);
-// console.log(sex)
-
-// const name = friends.filter(friend => friend.name);
-// console.log(name);
+ownForEach([1, 2, 3, 4, 5], logger);
 
 
 
-let arr = ['I', 'love', 'world'];
-   arr.splice(2, 0, 'this', 'beautiful');
 
-console.log(arr)
+
+
+
+
+
+
+
+// forEach
+// let array = [1, 2, 3, 4, 5]
+
+// const forEach = (array, logger) => {
+//    let newArray = [];
+//     for (let i = 0; i < array.length; i++){
+//         let item = array[i];
+//     }
+//     logger()
+
+// }
+// const logger = (item, index, array) => {
+//     console.log(`In array [${item}] on position ${index}: ${array}`);
+//   };
+//   newArray = forEach(array, logger);
+  
+
+
+// method map +
  
+// const ownMap = (arr, increment) => {
+//    let newArr = [];
+//    for(let i = 0; i < arr.length; i++){
+//       let newArr = item
+
+//       return item;
+
+   
+//     }
+//    const newArr = increment(item, i, arr);
+    
+//     increment = (item, i, arr) => item + 1;
+//    newArr = ownMap(arr, increment);
+    
+    
+// }
+
+// ownMap([1, 2, 3, 4, 5]);
+
+// function range (start, end, step) {
+//    if (!step) step = 1 // step size of zero makes no sense
+//    let arrayNum = [];
+//    // using ternary to decide stopping condition
+//    for (let i = start; step > 0 ? i <= end: i >= end; i += step ) {
+//      arrayNum.push(i);
+//    }
+//    return arrayNum;
+//  };
+ 
+// console.log(range(22, 34, 5))
+// console.log(range(10, -10, -2))
+// console.log(range(-5, -10, -1))
 
 
-let newArr = arr.map((item) => item.toUpperCase());
+    
+   
 
-const ownMap = (array, cb) => {
 
-}
 
-const callback = (item) => item.toUpperCase()
 
-newArr = ownMap(arr, callback)
+//   method filter +
+
+// let ar = [-1, 2, -3, -4, 5];
+ 
+// const ownFilter =(ar, isNegative ) => {
+    
+//     for (let i=0; i<ar.length; i++) {
+//     let item = ar[i];
+    
+//     isNegative();
+// }
+     
+// }
+// const isNegative = (item) => item < 0;
+// newAr = ownFilter(ar, isNegative())
+// console.log(newAr)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
