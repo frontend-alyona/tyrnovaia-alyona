1b8a5db9be085ea7f571d35bafd26542515b7f92
frontend-alyona <gluzhetskaya@gmail.com>
2021-09-02 13:52:50 +0300

    fixed

--- adv_homework13/script.js
+++ adv_homework13/script.js
@@ -1,25 +1,17 @@
-let btn_prev = document.querySelector('#gallery .buttons .prev');
-let btn_next = document.querySelector('#gallery .buttons .next');
+let btnPrev = document.querySelector('#gallery .buttons .prev');
+let btnNext = document.querySelector('#gallery .buttons .next');
 let images = document.querySelectorAll('#gallery .photos img');
 
 let i = 0; 
-    i++ ;
+    
 
 if(i >= images.length){
     i = 0; 
 
 }
 
-btn_prev.onclick = function(){
-    images[i].style.display = 'none';
-    i = i - 1;
-    if(i < 0){
-        i = images.length - 1;
-    }
-    images[i].style.display = 'block';
-}
 
-btn_next.onclick = function(){
+const handleClick = (event) => {
     images[i].style.display = 'none';
     i = i - 1;
     if(i < 0){
@@ -27,3 +19,7 @@ btn_next.onclick = function(){
     }
     images[i].style.display = 'block';
 }
+    btnPrev.addEventListener('click', handleClick) ;
+    btnNext.addEventListener('click', handleClick) ;
+
+
