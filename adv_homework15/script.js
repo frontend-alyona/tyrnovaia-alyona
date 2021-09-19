40e04afaa9674d9db4c06ba4b65ae4a8c1572b81
frontend-alyona <gluzhetskaya@gmail.com>
2021-09-19 13:56:09 +0300

    final commit

--- adv_homework15/script.js
+++ adv_homework15/script.js
@@ -28,13 +28,21 @@ const response = {
     },
   };
   
+  const {
+    meta:{
+      paging:{
+        total: getTotal,
+      }
+    },
+  } = response;
+  console.log(`getTotal`, getTotal);
 
 
-const isActive = response.data; 
-  console.log(`isActive`, isActive)
+  const getIsActive = response.data.filter(({is_active: isActive}) =>isActive === true  || isActive === false);
+  console.log(`getIsActive`, getIsActive);
+    
 
-const getTotal = response.meta.paging.total;
-  console.log(`getTotal`, getTotal)
+
 
   // task 2 +
 const user = {
@@ -52,17 +60,21 @@ const user = {
 // task 3 +
 
 const arr = [3, 11, 3, 4, 5, 28, 90],
-      arrMax = arr => arr.reduce((max,n) => n > max ? n : max)
+  arrMax = arr => arr.reduce((max,n) => n > max ? n : max)
       
 console.log(arrMax(arr));
 
 // task 4 +-
-const createMessage = ({author: guest, text, reciever, time = new Date()}) => {
-  return `From ${guest} to ${reciever}: ${text} (${new Date()})`;
+const createMessage = ({author = 'Guest',reciever, text, time = new Date()}) => {
+  return `From ${'Guest'} to ${reciever}: ${text} (${time})`;
 };
-const message = createMessage("Peter", "Sam", "Hello", new Date());
-console.log(`message`, message);
 
+const message = createMessage({
+  reciever: "John",
+  text: "Hi!",
+});
+
+console.log(`message`, message);
 
 
 // 5.1 +
@@ -71,10 +83,11 @@ let str = "x1y 722a 333 a123v1n a55555a qwe1 1zxc";
     return text.match (/\w\d+\w+/gi) 
   }
 const matches = findMatches(str);
-console.log(`matches`, matches)
+console.log(`matches`, matches);
 
-// task 5.2 +-
-const match = /^(?!\-)(?:[a-zA-Z\d\-]{0,62}[a-zA-Z\d]\.){1,126}(?!\d+)[a-zA-Z\d]{0,63}$/gm;
+// task 5.2 +
+
+const match = /([A-z]+)(\.)([a-z]{2,})/;
 console.log(`match.test('ex.ua')`, match.test('ex.ua'));
 console.log(`match.test('google.com')`, match.test('google.com')) ;
 console.log(`match.test('yandex.ru')`, match.test('yandex.ru'));
@@ -82,11 +95,12 @@ console.log(`match.test('site.com.ua')`, match.test('site.com.ua'));
 console.log(`match.test('my-page.com')`, match.test('my-page.com'));
 
 // 5.3 +
-let strOfNum = "7, 903, 123, 45, 5, 67,59,9393,04,797,0,04";
-
-let numbers = strOfNum.match(/\d{1,}/g);
-
-console.log(`numbers`, numbers) ;
+const regex = /^[0-9]{12,}$/;
+console.log(`regex.test('23468898765432')`, regex.test('23468898765432'));      
+console.log(`regex.test('285083549607g')`, regex.test('285083549607g'));
+console.log(`regex.test('4658')`, regex.test('4658'));
+console.log(`regex.test('287539065189673')`, regex.test('287539065189673'));
+console.log(`regex.test('fhjd393938575648kk')`, regex.test('fhjd393938575648kk'))
 
 
 
