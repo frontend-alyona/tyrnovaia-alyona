9a3bc3d3cde349a2a8a6a10ff63bffcba500baf3
frontend-alyona <gluzhetskaya@gmail.com>
2021-09-21 15:55:08 +0300

    fixed 1

--- adv_homework17/script.js
+++ adv_homework17/script.js
@@ -23,41 +23,48 @@ function displayCount() {
   timer--;
 }
 
+// timer--;
+
 
 const startBtn = document.querySelector('.start');
 const stopBtn = document.querySelector('.stop');
 
-
-
-startBtn.addEventListener('click', () => {
+const handleStart = (showTimeEnd) => {
   stopWatch = setInterval(displayCount, 1000);
   startBtn.disabled = true;
+
+  return new Promise ((resolve) => {
+    setTimeout(() => {
+      resolve("Time is up!")
+    }, showTimeEnd)
+  })
+}
+handleStart(6000).then(() => {
+  console.log(`Time is up!`)
 });
 
+// let promise = new Promise (function(resolve) {
+//   setTimeout(() => resolve("Time is up!"), 7000);
+// });
 
-stopBtn.addEventListener('click', () => {
+// promise.then(result => {
+//   console.log(`result`, result)
+// })
+
+const handleStop = () => {
   clearInterval(stopWatch);
   startBtn.disabled = false;
 
-  
-});
+ 
+}
+
+startBtn.addEventListener('click', handleStart);
+stopBtn.addEventListener('click', handleStop);
 
 
 
 displayCount();
 
-// const showTimeEnd = (displayCount) => {
-//     return new Promise((resolve) => {
-//       if(secondCount === 0){
-//         showTimeEnd(7000).then(() => console.log('Time is up'));
-//       }
-//       setTimeout(() => {
-//         resolve()
-//       }, displayCount)
-//     })
-//   }
-  
-  // showTimeEnd(7000).then(() => console.log('Time is up'))
 
 
 
